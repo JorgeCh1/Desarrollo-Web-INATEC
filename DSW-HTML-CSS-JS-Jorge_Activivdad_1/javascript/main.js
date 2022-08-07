258ad2c2c6f65cd6f6e57862653fd95aa10ad09c
JorgeCh1 <79027462+JorgeCh1@users.noreply.github.com>
2022-08-06 21:17:34 -0600

    Javascript 1

---
+++ DSW-HTML-CSS-JS-Jorge_Activivdad_1/javascript/main.js
@@ -0,0 +1,7 @@
+var alertaperso=document.getElementById("alertaperso");
+function mostrarAlerta(){
+    alertaperso.style.display='block';
+}
+function eliminarAlerta(){
+    alertaperso.style.display='none';
+}
